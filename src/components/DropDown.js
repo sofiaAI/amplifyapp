188ba996e616170e5a17f6424bf61238c6cd9b6f
Sofia <sofiam@mit.edu>
2021-06-30 23:56:01 -0400

    button fix

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -16,9 +16,9 @@ class DropDown extends React.Component {
         return (
           <div className = "drop-down">
             <div className = "drop-down-button"> Button 1 </div>
-            <div className = "drop-down-button"> Button 1 </div>
-            <div className = "drop-down-button"> Button 1 </div>
-            <div className = "drop-down-button"> Button 1 </div>
+            <div className = "drop-down-button"> Button 2 </div>
+            <div className = "drop-down-button"> Button 3 </div>
+            <div className = "drop-down-button"> Button 4 </div>
           </div>
         );
       }
