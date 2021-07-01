cda0dee0a20802e7a825830b2bcd817427a370f3
Sofia <sofiam@mit.edu>
2021-06-30 20:52:36 -0400

    logout button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,9 +13,12 @@ class NavBar extends Component {
 
   render() {
     return (
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <nav className="NavBar-container">
         <div className="NavBar-linkContainer u-inlineBlock">
-          <AmplifySignOut />
+          <i class="fas fa-sign-out-alt"></i>
+          <AmplifySignOut button-text= <i class="fa fa-sign-out"> />
+
         </div>
       </nav>
     );
