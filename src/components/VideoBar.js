74a82c715686db60e4ee5c81b5a256c12d1b4139
muthuku <67440419+muthuku@users.noreply.github.com>
2021-07-14 12:55:37 -0400

    working video switch

--- src/components/VideoBar.js
+++ src/components/VideoBar.js
@@ -16,7 +16,14 @@ class VideoBar extends Component {
     var button = buttonContainer.getElementsByClassName('live-feed-ret')[0];
     
     videoBar.classList.remove('video-bar-slide-anim');
-    buttonContainer.classList.remove('button-slide-anim');     
+    buttonContainer.classList.remove('button-slide-anim');
+    
+    var liveStream = document.getElementsByClassName('live-stream')[0];
+    var video = document.getElementsByClassName('video-play')[0];
+    
+    //replay clip in video box
+    liveStream.style.display = "block";
+    video.style.display = "none";     
   }
 
   render() {
